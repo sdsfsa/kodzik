bedab96f1d2f9536a3ab21d6f48feb4d762a9c57
bozo <mateusz71363@gmail.com>
2023-09-28 10:50:13 +0200

    *** *****

---
+++ wtf/src/Main.java
@@ -0,0 +1,36 @@
+                                                                                                            public class Main {public static void main(String[] args) {System.out.
+   println("Hello gitman!")
+                                                                                                            ;int
+   in = 1                                                                                                   ;double
+   d = 2.0                                                                                                  ;long
+   l = 2L
+                                                                                                            ;Integer
+   i2 = 3                                                                                                   ;int
+
+   i = 0                                                                                                    ;
+
+//   for ( i = 0; i < 4; i++)                                                                                 {System.out.
+//      println("for " + String.valueOf(i))                                                                   ;}
+//   while (i<8)                                                                                              {System.out.
+//      println("while " + String.valueOf(i))                                                                 ;
+//      i++                                                                                                   ;};
+//   i=0;
+   do                                                                                                       {System.out.
+      print(String.valueOf(i) + " ")                                                                        ;if(i==10){System.out.
+      print("\nHello\n")                                                                                    ;}
+      i                                                                                                     ++;}
+   while (i<=10)                                                                                            ;
+   a()                                                                                                      ;
+   b()                                                                                                      ;
+   c()                                                                                                      ;}private static void
+a()                                                                                                         {System.out.
+   println("Func a");
+                                                                                                            }public static void
+b()                                                                                                         {System.out.
+   println("Func b")
+                                                                                                            ;} protected static void
+c()                                                                                                         {System.out.
+   println("Func c")
+                                                                                                            ;}
+                                                                                                            }
+
